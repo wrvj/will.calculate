72b959a2e5de2cdbf797a19c8de7c3f4d0bf0ea2
Wilson Vitorino <wiljunior33@gmail.com>
2021-03-10 01:04:26 -0300

    finished engine version 3

--- calculator.js
+++ calculator.js
@@ -1,67 +1,45 @@
 class Calculator {
+
     constructor() {
-
-        this.state = Calculator.INITIAL_STATE;
-        this.display = { mainDisplay: "0", secondaryDisplay: "0" };
+      this.resetCalculator();
     }
-    static validKeys = /[\*\+\x2D-\/=(0-9)cC]/;
-    static operatorKeys = /(\+|\-|\/|\*)/;
-
-    static INITIAL_STATE = {expression : ['0'], rawInput : '0'};
     
+    static VALID_KEYS = /\*|\/|\+|\-|\>|\=|\.|[(0-9)]/;
+    static OPERATOR_KEYS = /(\+|\-|\/|\*|\>)/;
 
-    static operations = {
+    static INITIAL_EXPRESSION = '0';
+    static INITIAL_RAWINPUT = '0';
+    static INITIAL_INPUT = '0';
+    static INITIAL_HISTORY = '0';
+    
+    static OPERATIONS = {
         '+': (a, b) => { return (parseFloat(a) + parseFloat(b)) },
         '-': (a, b) => { return (parseFloat(a) - parseFloat(b)) },
         '*': (a, b) => { return (parseFloat(a) * parseFloat(b)) },
-        '/': (a, b) => { return (parseFloat(a) / parseFloat(b)) }
+        '/': (a, b) => { return (parseFloat(a) / parseFloat(b)) },
+        '>': (a, b) => { return parseFloat(b) }
     }
-    static calculate(a, operation, b) {
-        try {
-            return Calculator.operations[operation](a, b);
-        } catch (e) {
-            return undefined
-        }
 
+    enterKey(rawKey){
+      if (this.isResetKey(rawKey)) { this.resetCalculator() }
+      else if (this.isValidKey(rawKey)) { this.handleKey(rawKey) }
     }
-    enterKey(key){
-      if(!this.isValidKey(key)){
-        return;
-      }
+    handleKey(key){
+
       this.state.rawInput = this.addToRawInput(key);
-      
-      if(this.isEqualKey(key)) {
-        this.state.rawInput = '0' +  this.solveExpression(this.state.expression).toString();
-        // this.state.expression = [this.solveExpression(this.state.expression)];
-      } 
-      if (this.isResetKey(key)){
-        this.state.rawInput = '0';
-      }
-
       this.state.expression = this.processRawInput(this.state.rawInput);
-      this.display.secondaryDisplay = this.state.expression.join(' ').toString() + ` = ${this.solveExpression(this.state.expression)}`;
       
       if (this.isNumber(key))this.display.mainDisplay = this.state.expression[this.state.expression.length-1];
-      if(this.isOperator(key) || this.isEqualKey(key) || this.isResetKey(key)){this.display.mainDisplay = this.solveExpression(this.state.expression)}
-      console.log(this.state.rawInput, this.state.expression, this.solveExpression(this.state.expression).toString());
+      if (this.isOperator(key) || this.isEqualKey(key) || this.isResetKey(key)){this.display.mainDisplay = this.solveExpression(this.state.expression)}
+      if (this.isEqualKey(key)) {
+        this.state.rawInput += '>' + this.solveExpression(this.state.expression).toString() + '>';
+      } 
+      this.display.secondaryDisplay = this.state.expression.join(' ').toString() + ` = ${this.solveExpression(this.state.expression)}`;
     }
-    isNumber(element){
-      return (parseFloat(element) || parseFloat(element) === 0 || element==='.') ? true : false;
-    }
-    isOperator(element){
-      return Calculator.operatorKeys.test(element);
-    }
-    isEqualKey(key){
-      return /=/.test(key);
-    }
-    isResetKey(key){
-      return /c|C/.test(key);
-    }
-    isValidKey(key){
-      return Calculator.validKeys.test(key);
-    }
-    addToRawInput(char){
-        return this.removeOperatorDuplicates(this.state.rawInput + char);
+    addToRawInput(key){
+        if(!/\./.test(this.state.expression[this.state.expression.length-1]) || key != '.')
+        return this.removeOperatorDuplicates(this.state.rawInput + key).replace('=', '');
+        else return this.state.rawInput;
     }
     removeOperatorDuplicates(rawInput){
       let newRawInput = '';
@@ -72,23 +50,43 @@ class Calculator {
       }
       return newRawInput;
     }
-
     processRawInput(rawInput){
-      return rawInput.split(Calculator.operatorKeys).map(x => this.customParseFloat(x));
+      return rawInput.split(Calculator.OPERATOR_KEYS).map(x => this.customParseFloat(x));
     }
     customParseFloat(element){
-      return (this.isNumber(element)) ? parseFloat(element) : element;
+      return (this.isNumber(element)) ? this.removeLeftZeros(element) : element;
     }
-
-
+    removeLeftZeros(number) {
+      if (number.charAt(0) == '0' && number.charAt(1) != '.' && number.length > 1) return this.removeLeftZeros(number.slice(1))
+      else return number;
+  }
+    resetCalculator(){
+    this.state = {expression: [Calculator.INITIAL_EXPRESSION], rawInput: Calculator.INITIAL_RAWINPUT};
+    this.display = {mainDisplay: Calculator.INITIAL_INPUT, secondaryDisplay : Calculator.INITIAL_HISTORY};
+  }
     solveExpression(expression){
       let accumulator = expression[0];
       for (const [i,x] of expression.entries()){
         if(this.isOperator(x) && this.isNumber(expression[i+1])){
-          accumulator = Calculator.operations[x](accumulator, expression[i+1]);
+          accumulator = Calculator.OPERATIONS[x](accumulator, expression[i+1]);
         } 
       }
       return accumulator;
+    }
+    isNumber(element){
+      return (parseFloat(element) || parseFloat(element) === 0 || element==='.') ? true : false;
+    }
+    isOperator(element){
+      return Calculator.OPERATOR_KEYS.test(element);
+    }
+    isEqualKey(key){
+      return /=/.test(key);
+    }
+    isResetKey(key){
+      return /c|C/.test(key);
+    }
+    isValidKey(key){
+      return Calculator.VALID_KEYS.test(key);
     }
 }
 
